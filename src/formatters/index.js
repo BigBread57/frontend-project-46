58fc1da4e95548c2a6a4cb04675875dd72b8b36f
Gleb Mazur <g.mazur@samolet.ru>
2025-12-05 15:25:39 +0300

    Удаление кавычек

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,8 +3,10 @@ import { plainDiff } from './plain.js'
 
 export const formatters = (raw_result, format) => {
   switch (format) {
-    case 'stylish':
-      return JSON.stringify(raw_result.reduce(stylishCallback, {}), null, 3)
+    case 'stylish': {
+      const jsonString = JSON.stringify(raw_result.reduce(stylishCallback, {}), null, 4)
+      return jsonString.replace(/"([^"]*)/g, '$1')
+    }
     case 'plain':
       return plainDiff(raw_result)
     case 'json':
