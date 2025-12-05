bf1d3c40a19d34b0859219401220bc89d3d909d7
Gleb Mazur <g.mazur@samolet.ru>
2025-12-05 15:16:28 +0300

    Правка отступов

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -4,7 +4,7 @@ import { plainDiff } from './plain.js'
 export const formatters = (raw_result, format) => {
   switch (format) {
     case 'stylish':
-      return JSON.stringify(raw_result.reduce(stylishCallback, {}), null, 4)
+      return JSON.stringify(raw_result.reduce(stylishCallback, {}), null, 3)
     case 'plain':
       return plainDiff(raw_result)
     case 'json':
